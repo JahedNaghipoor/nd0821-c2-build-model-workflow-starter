590fb7ffc6c5e6b14aa72a45c19ab483566f3433
jahed.naghipoor <jahed.naghipoor@alexanderthamm.com>
2022-01-05 12:05:09 +0100

    Modify minor issues

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -44,7 +44,6 @@ def go(args):
     run.config.update(args)
 
     # Get the Random Forest configuration and update W&B
-    # print(args.rf_config)
     try:
         with open(args.rf_config) as fp:
             rf_config = json.load(fp)
